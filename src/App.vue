4bba3f58b8c14c8b6fd8ce47789cb60809c4f566
Art Furrer <2823246+bonham@users.noreply.github.com>
2022-03-20 20:21:45 +0100

    after initial config

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Navbar/>
+    <wsearch-form/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from './components/Navbar.vue'
+import WsearchForm from './components/WsearchForm.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    WsearchForm
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

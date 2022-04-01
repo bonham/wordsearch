a2070c5969c7af17cf94bb02bc389b630f3967ff
Art Furrer <2823246+bonham@users.noreply.github.com>
2022-04-01 20:11:40 +0200

    elimitate one component level

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,91 @@
       @reset="resetAction"
       @lang="setLang"
     />
-    <wsearch-form
-      :lang="langStatus"
-      :reset-trigger="resetCounter"
-    />
+    <div class="container d-flex flex-column shadow">
+      <div class="rounded border p-3 mt-2 mb-1">
+        <CharAnyPositionForm 
+          :reset-trigger="resetCounter"
+          @anypos-form-update="processCharAnyPositionConstraints"
+        />
+        <CharPositionForm 
+          :reset-trigger="resetCounter"
+          @pos-form-update="processPositionConstraints"
+        />
+      </div>
+
+      <ResultText 
+        :words-text="wordsText"
+        :result-length="resultLength"
+        :lang="lang"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import Navbar from './components/nav-bar.vue'
-import WsearchForm from './components/WsearchForm.vue'
+import CharPositionForm from '@/components/CharPositionForm'
+import CharAnyPositionForm from '@/components/CharAnyPositionForm'
+import { wordLists } from "@/lib/wordlists"
+import ResultText from '@/components/ResultText'
+import Navbar from '@/components/nav-bar'
+
 export default {
   name: 'App',
   components: {
     Navbar,
-    WsearchForm
+    CharAnyPositionForm,
+    CharPositionForm,
+    ResultText,
   },
   data() {
     return {
+      lang: "EN",
       resetCounter: 0,
-      langStatus: "EN"
+      anychar: "",
+      positionConstraints: {},
     }
   },
   methods: {
+
     resetAction() {
+      this.positionConstraints = {}
+      this.anychar = ""
       this.resetCounter++
     },
+
     setLang(language) {
       this.resetAction()
-      this.langStatus = language
+      this.lang = language
+    },
+
+    processCharAnyPositionConstraints(e) {
+      this.anychar = e
+    },
+
+    processPositionConstraints(e) {
+      this.positionConstraints = e
+    },
+  },
+
+  computed: {
+    workingWordList() {
+      const workingWordList = wordLists[this.lang].clone()
+      workingWordList.reduceAnyCharInString(this.anychar)
+
+      for (const p in this.positionConstraints) {
+        const c = this.positionConstraints[p]
+        workingWordList.reduceCharAtPosition(c, p)
+      }
+
+      return workingWordList
+    },
+
+    resultLength() {
+      return this.workingWordList.len()
+    },
+
+    wordsText() {
+      return this.workingWordList.getArray().join(" ")
     }
   }
 }
